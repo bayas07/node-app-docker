c2f75ca9db5a43755be4e4fd6abcf2aaddd135bf
Byas Ahamed <byas.ahamed@academy.com>
2023-09-26 16:07:33 +0530

    Accessing multiple prop files

--- src/index.js
+++ src/index.js
@@ -1,44 +1,40 @@
 const express = require("express");
-const fs = require("fs");
 const chokidar = require("chokidar");
 const PropertiesReader = require('properties-reader');
 const app = express();
 
-const getConfigProperties = () => {
-  const properties = PropertiesReader('/config/config.properties');
-  return {
-      greeting: properties.get('greeting') || "Default Greeting",
-      name: properties.get('name') || "Default Name",
-      city: properties.get('city') || "Default city"
-      // Add more properties as needed.
-  };
+const readPropertyFile = (path) => {
+    const properties = PropertiesReader(path);
+    return properties.getAllProperties();
 };
 
-let { greeting, name, city } = getConfigProperties();
+let config = { ...readPropertyFile('/config/config.properties'), ...readPropertyFile('/config/configNew.properties') };
 
-const watcher = chokidar.watch('/config/config.properties', {
+const watcher = chokidar.watch('/config', {
     persistent: true
 });
 
 watcher.on('change', (path) => {
-  console.log(`File ${path} has been changed`);
-  let updatedConfig = getConfigProperties();
-  greeting = updatedConfig.greeting;
-  name = updatedConfig.name;
-  city = updatedConfig.city;
-  // If you add more properties, update them here as well.
-  console.log('Updated Config:', updatedConfig);
+    console.log(`File ${path} has been changed`);
+
+    // Update the relevant properties based on the changed file
+    if (path.endsWith('config.properties')) {
+        config = { ...config, ...readPropertyFile(path) };
+    } else if (path.endsWith('configNew.properties')) {
+        config = { ...config, ...readPropertyFile(path) };
+    }
+
+    console.log('Updated Config:', config);
 });
 
 app.get('/', (req, res) => {
-    res.send(`App config prop success - Greeting: ${greeting}, Name: ${name}, City: ${city}`);  
+    res.send(`App config prop success - Greeting: ${config.greeting || "Default Greeting"}, Name: ${config.name || "Default Name"}, City: ${config.city || "Default city"}, isXCC: ${config.isXCC || 'false'}`);  
 });
 
 app.get('/config', (req, res) => {
-  res.send(`Greeting: ${greeting}, Name: ${name}, City: ${city}`);  
+    res.send(`Greeting: ${config.greeting || "Default Greeting"}, Name: ${config.name || "Default Name"}, City: ${config.city || "Default city"}, isXCC: ${config.isXCC || 'false'}`);
 });
 
 app.listen(3003, (err) => {
     if (!err) console.log('App is running');  
 });
-
